61424614f93c7291ccb2d159aa7e266b27733b1a
JinWook <qwert4932@naver.com>
2021-07-03 00:10:44 +0900

    Initial commit

---
+++ src/main/java/com/devjin/muv/sample/SampleHotel.java
@@ -0,0 +1,14 @@
+package com.devjin.muv.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor
+public class SampleHotel {
+    private Chef chef;
+}
